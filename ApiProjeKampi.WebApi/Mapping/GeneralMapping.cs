511db234b0cf0f40f4649ee6cc78db5c58a8dc7a
Nazli2 <deniznazlisahin@gmail.com>
2025-08-21 17:19:28 +0300

    Ders 20 sonu

--- ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -1,5 +1,6 @@
 ﻿using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
 using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Dtos.ProductDtos;
 using ApiProjeKampi.WebApi.Entities;
 using AutoMapper;
 
@@ -18,5 +19,8 @@ public class GeneralMapping : Profile
         CreateMap<Message, CreateMessageDto>().ReverseMap();
         CreateMap<Message, UpdateMessageDto>().ReverseMap();
         CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+
+        CreateMap<Product, CreateProductDto>().ReverseMap();
+        CreateMap<Product, ResultProductWithCategoryDto>().ForMember(x => x.CategoryName, y => y.MapFrom(z => z.Category.CategoryName)).ReverseMap();
     }
 }
---
+++ ApiProjeKampi.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.Controllers;
+public class DefaultController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
