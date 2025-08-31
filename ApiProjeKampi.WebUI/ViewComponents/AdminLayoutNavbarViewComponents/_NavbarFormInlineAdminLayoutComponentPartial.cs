1390cb8cbdc3a8489b30011a32f421b62818ba3c
Nazli2 <deniznazlisahin@gmail.com>
2025-08-31 23:41:10 +0300

    Admin Layoutta Düzenlemeler

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarFormInlineAdminLayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents;
+
+public class _NavbarFormInlineAdminLayoutComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
