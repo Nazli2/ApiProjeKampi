4c84d4c544b719a4d6ec7e42fec8218dfb77436b
Nazli2 <deniznazlisahin@gmail.com>
2025-09-08 14:41:03 +0300

    Navbar İşlemlerinin Tamamlanması

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarAccountMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents;
+
+public class _NavbarAccountMenuAdminLayoutComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
