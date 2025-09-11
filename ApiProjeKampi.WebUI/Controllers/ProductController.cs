d790215131575f20dc0da5709c60a982d6729df8
Nazli2 <deniznazlisahin@gmail.com>
2025-09-11 19:16:01 +0300

    Ürün / Kategori İlişkisi Api ve Consume

--- ApiProjeKampi.WebUI/Controllers/ProductController.cs
+++ ApiProjeKampi.WebUI/Controllers/ProductController.cs
@@ -18,7 +18,7 @@ public class ProductController : Controller
     public async Task<IActionResult> ProductList()
     {
         var client = _httpClientFactory.CreateClient();
-        var responseMessage = await client.GetAsync("https://localhost:7189/api/Products");
+        var responseMessage = await client.GetAsync("https://localhost:7189/api/Products/ProductListWithCategory");
         if (responseMessage.IsSuccessStatusCode)
         {
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
--- ApiProjeKampi.WebUI/Dtos/ProductDtos/ResultProductDto.cs
+++ ApiProjeKampi.WebUI/Dtos/ProductDtos/ResultProductDto.cs
@@ -8,4 +8,5 @@ public class ResultProductDto
     public decimal Price { get; set; }
     public string ImageUrl { get; set; }
     public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
 }
